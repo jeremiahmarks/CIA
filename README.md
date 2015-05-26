14869751bc638eaee0ee2b0bf80c00dcd2536820
Jeremiah Marks <jeremiah@jlmarks.org>
2015-05-26 11:04:49 -0700

    Added documentation to copier class
    Also removed file that contained the same code, but only partially
    implemented.

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 ##The CSV Import Assistant
 
-Welcome to the CIA!  This is a more directed collection of 
+Welcome to the CIA!  This is a more directed collection of
 tools and scripts intended to help out with managing CSV files,
 matching records between CSV files, and importing records to
 Infusionsoft.
 
-`CIA.BlackOps` is probably best left alone, it is where 
+`CIA.BlackOps` is probably best left alone, it is where
 special projects like to try and make their home.
 
 The CIA has three basic components:
 
 * CSV manipulation
-    * Basically a suite of tools that work specifically on 
+    * Basically a suite of tools that work specifically on
     CSV files
     * Example Functions include:
         * Break up files to set size limit:
@@ -29,21 +29,21 @@ The CIA has three basic components:
 * Import functions
     * provide interaction with the Infusionsoft API to pull
     existing records for duplication avoidance
-    * provide ability to perform imports not available, for 
+    * provide ability to perform imports not available, for
     instance, product options
     * depending on the scope of the import, browser automation
     will also live here
 * Assistant(007)
     * the assistant will start by creating a project for you
-    or loading an existing project, if so directed. 
+    or loading an existing project, if so directed.
     * Each project can consist of many files.
-    * The assistant will use the tools available to perform 
+    * The assistant will use the tools available to perform
     tasks.
-        * EX1: 
+        * EX1:
             * Accept a csv file of contact information.
             * Find which columns would indicate a duplicate.
             * provide a list in both html and csv of all
-            duplicate entries found. 
+            duplicate entries found.
             Depending on size of result and user input either
                 * Do it
                     * create the custom fields required then
@@ -59,23 +59,23 @@ The CIA has three basic components:
             * Check for existance of listed products, create
             if needed, update dependencies as needed.
             * Check for existance of listed product categories,
-            create as needed. 
+            create as needed.
             * Check for record of relationship with products
             assigned to category, if none exists, create one.
-            * Check for record of product option. Create as 
+            * Check for record of product option. Create as
             needed.
         * notes about examples: If this is a large data set,
-        it can hit the API pretty hard. Since the assistant 
+        it can hit the API pretty hard. Since the assistant
         uses csv files (and will be able to use a local sql
         database) if the project needs to be stopped, as long
         as you leave the files in place, the assistant will
-        be able to pick up where he left off. 
+        be able to pick up where he left off.
 
 #Image Import
 
 I really think that this is the coolest thing since sliced
 bread. I mean, it is pretty cool. But it is time consuming.
-I am looking at other options Specifically 
+I am looking at other options Specifically
 
 * [MechanicalSoup](https://github.com/hickford/MechanicalSoup)
 * [RoboBrowser](https://github.com/jmcarp/robobrowser)
@@ -83,8 +83,8 @@ I am looking at other options Specifically
 By "Looking at" I mean I have read the README.
 
 Anyways, image import will accept a CSV file with a relationship
-to a product. When it can determine what it's URL is (by the 
-product ID being known (API lookup or csv file)) it will open a 
+to a product. When it can determine what it's URL is (by the
+product ID being known (API lookup or csv file)) it will open a
 browser, upload the image, navigate to the product on the store
 front, take a screenshot, and then do the next in queue or close
 the browser.
--- tools/CSVToolBox/Copier.py
+++ tools/CSVToolBox/Copier.py
@@ -3,18 +3,18 @@
 # @Author: Jeremiah Marks
 # @Date:   2015-05-24 16:10:08
 # @Last Modified 2015-05-26
-# @Last Modified time: 2015-05-26 01:51:43
+# @Last Modified time: 2015-05-26 11:03:13
 
 import os
 import csv
 import CIA.tools.credentials
 
 class Copier(object):
-    """Extensible class (Perhaps should be a imaginary 
+    """Extensible class (Perhaps should be a imaginary
     object, whatever those are called.) As a standalone,
     will accept the path to a csv file. It will then open
     the file and copy it to the specified location. In the
-    transition, child classes will either manipulate of 
+    transition, child classes will either manipulate of
     process the information on the way.
     """
     def __init__(self, pathtocsv, destination):
@@ -41,8 +41,8 @@ class Copier(object):
         self.closefiles()
 
     def createdestination(self):
-        """Checks if the path exists to the destination 
-        folder. If it does not, it creates it. 
+        """Checks if the path exists to the destination
+        folder. If it does not, it creates it.
         """
         destFolder=os.path.dirname(os.path.abspath(self.destination))
         if not os.path.isdir(destFolder):
@@ -60,39 +60,54 @@ class Copier(object):
         self.inputreader=csv.DictReader(self.inputcsv)
         self.outputwriter=csv.DictWriter(self.outputcsv,self.inputreader.fieldnames)
 
-class addrow(Copier):
+class ColAdder(Copier):
+    """Col. Adder will add a new columd to your CSV file and
+    populate it with a randomly generated string.
+    Since it is an extension of Copier, when this class is
+    called it is initialiaed by the Copier __init__, which
+    then calls this classes act function, which behaves
+    differently than the originals.
+    """
     def act(self):
         self.createdestination()
         self.openinputfile()
         self.getinputmanipulator()
         self.inputrows=[]
+         # Ensures that the destination is created then
+         # opens the input file and gets a CSV Dictreader
+         #for it.
         for row in self.inputreader:
             row["LOCALID"]=CIA.tools.credentials.getRandomID()
             self.inputrows.append(row)
+            # This for loop basically adds a key and value
+            # for that key to each record from the input file
         self.closeinputfile()
         self.openoutputfile()
+        # Close the input, then open the output file
+        # This should mean that the method can use the same
+        # File.
+
         self.newfieldnames=self.inputrows[0].keys()
         self.getoutputmanipulator()
         self.outputwriter.writeheader()
+        # Here we get new list of headers, get an output
+        # writer, and have it add the headers (fieldnames)
+        # to the output CSV
+
         for eachRow in self.inputrows:
             self.outputwriter.writerow(eachRow)
+            # Writing the new CSV file to file.
         self.closeoutputfile()
-
-
-
-
 
     def openinputfile(self):
         self.inputcsv=open(self.origcsv)
     def closeinputfile(self):
         self.inputcsv.close()
 
-
     def openoutputfile(self):
         self.outputcsv=open(os.path.abspath(self.destination),'wb+')
     def closeoutputfile(self):
         self.outputcsv.close()
-
 
     def getinputmanipulator(self):
         self.inputreader=csv.DictReader(self.inputcsv)
@@ -100,34 +115,4 @@ class addrow(Copier):
         self.outputwriter=csv.DictWriter(self.outputcsv,self.newfieldnames)
 
 
-# def addRowNumAsCol(pathToCSV, pathtostorage=None):
-#     """Accepts the absolute path to a csvFile
-#     """
-#     if not os.path.exists(pathToCSV):
-#         return False
-#     filename=os.path.basename(pathToCSV)
-#     newFile=os.path.join(os.path.dirname(os.path.abspath(pathToCSV)), "updatedWithRowCounter"+filename)
-#     outFile=open(newFile,'wb+')
-#     csvFile=open(pathToCSV,'rb+')
-#     reader=csv.DictReader(csvFile)
-#     allRows=[]
-#     for rowNum,row in enumerate(reader):
-#         while None in row.keys():
-#             garbage=row.pop(None)
-#         row["rowNum"]=rowNum
-#         allRows.append(row)
-#     csvFile.close()
-#     allH=set()
-#     for eachRow in allRows:
-#         now=len(allH)
-#         allH.union(set(eachRow.keys()))
-#         later=len(allH)
-#         if later>now:
-#             print allH
-#     print allRows[0].keys()
-#     writer=csv.DictWriter(outFile, allRows[0].keys())
-#     writer.writeheader()
-#     for eachRow in allRows:
-#         writer.writerow(eachRow)
-#     outFile.close()
-#     return newFile
+class ColCleaner(Copier):
