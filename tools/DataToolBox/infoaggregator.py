051db75bf983e8aee3c1f3f5dd664a32b0039c7b
Jeremiah Marks <jeremiah@jlmarks.org>
2015-05-27 13:54:49 -0700

    processed the same data in one less iteration
    moved the colstats variable from its own loop to the loop that builds
    colvals.  additionally added a numofresultsreturned variable that the
    method can accept to return only a limited

--- tools/DataToolBox/infoaggregator.py
+++ tools/DataToolBox/infoaggregator.py
@@ -3,10 +3,10 @@
 # @Author: jeremiah.marks
 # @Date:   2015-05-26 13:48:24
 # @Last Modified by:   jeremiah.marks
-# @Last Modified time: 2015-05-26 18:03:20
+# @Last Modified time: 2015-05-27 13:51:47
 import csv
 
-def countuniquevalues(pathtofile):
+def countuniquevalues(pathtofile, numofresultsreturned=10):
     """This method accepts the path to a file. From there
     it creates a dictionary, named colvals to hold the
     values for each column.
@@ -22,6 +22,7 @@ def countuniquevalues(pathtofile):
     and assign it an empty list.
     It will then append the row number to the list.
     """
+    colstats={}
     colvals={}
     inputfile=open(pathtofile)
     reader=csv.DictReader(inputfile)
@@ -32,40 +33,15 @@ def countuniquevalues(pathtofile):
             if eachcolumnname and len(eachcolumnname.strip())>0:
                 if eachcolumnname not in colvals.keys():
                     colvals[eachcolumnname]={}
+                    colstats[eachcolumnname]={}
                 if len(row[eachcolumnname].strip())>0:
                     if row[eachcolumnname] not in colvals[eachcolumnname].keys():
                         colvals[eachcolumnname][row[eachcolumnname]]=[]
+                        colstats[eachcolumnname][row[eachcolumnname]]=0
                     colvals[eachcolumnname][row[eachcolumnname]].append(rowCounter)
+                    colstats[eachcolumnname][row[eachcolumnname]]+=1
     inputfile.close()
-    # now we have a data structure that looks like this:
-        # colvals = {
-            # rowheader1: {
-                # val1: [1,3,4,7]
-                # val2: [2,5,6]
-            # }
-            # rowheader2: {
-                # val3: [1,2,3]
-                # val4: [4, 5, 6, 7]
-            # }
-        # }
-    # general stats:
-    # len(rowheader1.keys()) will tell you how many unique
-    # values there were in the column named rowheader1
-    # len(rowheader1[val1]) will tell you how many records
-    # have that value in the column
-    # Lets find and print that
-    colstats={}
-    # colstats['colname']=''
-    # colstats['uniqval']=int(number of unique values)
-    for eachcolumnname in colvals.keys():
-        if eachcolumnname not in colstats.keys():
-            colstats[eachcolumnname]={}
-        for eachvalue in colvals[eachcolumnname].keys():
-            colstats[eachcolumnname][eachvalue]=len(colvals[eachcolumnname][eachvalue])
     tabledata=[]
     for eachcol in colstats.keys():
-        tabledata.append([eachcol, sorted(colstats[eachcol].items(), key=lambda x: x[1])])
+        tabledata.append([eachcol, sorted(colstats[eachcol].items(), key=lambda x: x[1])[ -numofresultsreturned:]])
     return tabledata
-
-
-
